dd0687456f00be36b495a0dfa1aad497fb48eb9b
Anibal <al.anibalmarcos@iesramonllull.net>
2023-11-02 12:43:00 +0100

    second commit

--- main.py
+++ main.py
@@ -24,9 +24,36 @@ print(texto + " - " + nombre + " - " + altura + " - " + str(year))
 #print("El sitio web del usuario es: " + sitioweb)
 
 # Condiciones
+"""
 altura = int(input("¿Cuál es tu altura?: "))
 
 if altura >= 180:
     print("Eres una persona alta")
 else: 
     print("Eres bajito")
+"""    
+
+# Funciones
+"""
+var_altura = int(input("¿Cuál es tu altura?: "))
+
+def mostrarAltura(altura):
+    resultado = ""
+
+    if altura >= 180:
+         resultado = "Eres una persona alta"
+    else: 
+         resultado = "Eres bajito"
+
+    return resultado
+
+print(mostrarAltura(var_altura))
+"""
+
+# Listas
+personas = ["Víctor","Paco", "Pepe"]
+
+print(personas[2])
+
+for persona in personas:
+    print("-" + persona)
