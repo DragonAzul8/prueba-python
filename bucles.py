1cc61caa13c4a2619cf91889fe72476a1ba7da8f
Anibal <al.anibalmarcos@iesramonllull.net>
2023-11-16 12:36:55 +0100

    third commit

--- bucles.py
+++ bucles.py
@@ -1,4 +1,30 @@
 # Bucles
-
-for numero in range(1000000):
+"""
+for numero in range(10, 30):
     print("Hola", numero)
+
+fruta = "plátano"
+frutas = ["manzana", "pera"]
+frutas.append(fruta)
+
+#print(frutas[0])
+#print(frutas[1])
+#print(frutas[2])
+
+for frutaseleccionada in frutas:
+    if frutaseleccionada == "pera":
+        break
+    print(frutaseleccionada)
+
+sumatorio = 0
+for numero in range(1,101):
+    sumatorio = sumatorio + numero
+print(sumatorio)
+"""
+
+vida = 100
+while vida > 0:
+    vida -= 10
+    if vida > 30:
+        continue
+    print("te queda", vida)
---
+++ funciones.py
@@ -0,0 +1,8 @@
+def saludar(nombre, apellido):
+    print("Función saludar")
+    print("Hola", nombre, apellido)
+
+def calcualr_iva ():
+    
+
+saludar("Dani", "Moreno")
